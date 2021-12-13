211ebb744683bbee107f02ab4d5fe977da45fa2e
David Heap <davidheap.DH@hotmail.co.uk>
2021-12-13 16:12:16 +0000

    added bullet and enemy

--- tank-wars-2021/js/baseTank.js
+++ tank-wars-2021/js/baseTank.js
@@ -1,30 +1,43 @@
-class BaseTank{
+class BaseTank {
     /**@type {number} */
-    tankSpeed = 10
+    tankSpeed = 100
     /**@type {number} */
     damageMax = 2
-    constructor(scene, x, y, texture,frame){
+    constructor(scene, x, y, texture, frame) {
+        this.scene = scene
+        this.shadow = this.scene.add.image(x, y, texture, 'shadow')
+        this.shadow.setDepth(1)
+        this.hull = this.scene.physics.add.sprite(x, y, texture, frame)
+        this.hull.body.setSize(this.hull.width - 8, this.hull.height - 8)
+        this.hull.setDepth(2)
+        this.hull.body.collideWorldBounds = true
+        this.hull.body.bounce.setTo(1, 1)
+        this.turret = this.scene.physics.add.sprite(x, y, texture, 'turret')
+        this.turret.setDepth(4)
+        this.damageCount = 0
 
     }
-    update(){
+    update() {
+        this.shadow.x = this.turret.x = this.hull.x
+        this.shadow.y = this.turret.y = this.hull.y
+        this.shadow.rotation= this.hull.rotation
+    }
+    damage() {
 
     }
-    damage(){
+    setBullets() {
 
     }
-    setBullets(){
+    burn() {
 
     }
-    burn(){
+    isDestroyed() {
 
     }
-    isDestroyed(){
+    isImobillised() {
 
     }
-    isImobillised(){
-
-    }
-    enableCollision(){
-
+    enableCollision(destructLayer) {
+        this.scene.physics.add.collider(this.hull,destructLayer)
     }
 }
